1e98b591c3e90937665d318300460debd7aa8970
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-10-01 06:42:06 -0400

    TEST FOR Player class

--- src/main/java/project/PlayerTest.java
+++ src/main/java/project/PlayerTest.java
@@ -30,4 +30,20 @@ public class PlayerTest {
         Assert.assertEquals(0, p2.getCapacity());
     }
 
+    @Test
+    public void testInsertingTilesInHand() {
+        Tile t1 = new Tile("R", 1, 1, true);
+        Tile t2 = new Tile("B", 11, 10, true);
+        Tile t3 = new Tile("G", 13, 10, true);
+        Tile t4 = new Tile("0", 12, 10, true);
+
+        p2.insertTileInHand(t1);
+        p2.insertTileInHand(t2);
+        p2.insertTileInHand(t3);
+        p2.insertTileInHand(t4);
+
+        Assert.assertEquals(4, p2.getCapacity()); // no of tiles in hand
+    }
+
+
 }
