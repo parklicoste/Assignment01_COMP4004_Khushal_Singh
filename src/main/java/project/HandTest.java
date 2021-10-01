a52a9a0e4061f23e90097fcb5c12eb461a7d0f51
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-10-01 04:15:32 -0400

    TEST FOR Hand class

--- src/main/java/project/HandTest.java
+++ src/main/java/project/HandTest.java
@@ -8,9 +8,26 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class HandTest {
+
+    Hand h = new Hand();
+
     @Test
     public void testHand() {
-        Hand h = new Hand();
         Assert.assertEquals(Collections.emptyList(), h.getHand()); // check for deck
+    }
+
+    @Test
+    public void testInsertTileInHand() {
+        Tile t1 = new Tile("R", 1, 1, true);
+        Tile t2 = new Tile("B", 11, 10, true);
+        Tile t3 = new Tile("G", 13, 10, true);
+        Tile t4 = new Tile("0", 12, 10, true);
+
+        h.insert(t1);
+        h.insert(t2);
+        h.insert(t3);
+        h.insert(t4);
+
+        Assert.assertEquals(4, h.getHand().size()); // check for 4 tiles in hand
     }
 }
