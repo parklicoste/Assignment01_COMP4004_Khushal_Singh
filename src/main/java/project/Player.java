e9c3545d9c3f2048081c5a3385ab24d064cd8ed7
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-10-01 03:01:38 -0400

    CODE FOR Player class getting/setting Hand list

--- src/main/java/project/Player.java
+++ src/main/java/project/Player.java
@@ -4,13 +4,16 @@ import java.util.*;
 
 public class Player {
     private String name;
+    Hand player_hand;
 
     public Player() {
         this.name= "Bob";
+        this.player_hand = new Hand();
     }
 
     public Player(String n) {
         this.name = n;
+        this.player_hand = new Hand();
     }
 
     public String getName() {
@@ -19,5 +22,13 @@ public class Player {
 
     public void setName(String n) {
         this.name= n;
+    }
+
+    public void setPlayer_hand(ArrayList<Tile> list) {
+        this.player_hand.setHand(list);
+    }
+
+    public ArrayList<Tile> getPlayer_hand() {
+        return player_hand.getHand();
     }
 }
--- src/main/java/project/PlayerTest.java
+++ src/main/java/project/PlayerTest.java
@@ -3,12 +3,20 @@ package project;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+
 public class PlayerTest {
 
     @Test
     public void testPlayer() {
         Player p1 = new Player();
         Assert.assertEquals("Bob", p1.getName());
+    }
+
+    @Test
+    public void testHandOfPlayer() {
+        Player p1 = new Player();
+        Assert.assertEquals(Collections.emptyList(), p1.getPlayer_hand());
     }
 
 
