dae1461482d03d28f654e45888d227d52c872f7c
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-10-01 09:23:58 -0400

    CODE FOR Meld Class, run and group meld tested

--- src/main/java/project/Meld.java
+++ src/main/java/project/Meld.java
@@ -19,4 +19,51 @@ public class Meld {
     public ArrayList<ArrayList<Tile>> getRun() {
         return run;
     }
+
+    public boolean checkMeld(ArrayList<Tile> m){
+
+        boolean r ,g;
+        r = this.isRun(m);
+        g = this.isGroup(m);
+        return r || g;
+    }
+
+    public boolean isRun(ArrayList<Tile> r){
+        int len = r.size();
+
+        String tempSuite = r.get(0).getSuite();
+        int tempInt = r.get(0).getRank();
+
+        if(len >= 3 ){
+            for(int i=1; i<len; i++){
+                if(r.get(i).getSuite().equals(tempSuite) && r.get(i).getRank() - 1 == tempInt){
+                    tempInt = r.get(i).getRank();
+                }
+                else return false;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+
+
+    public boolean isGroup(ArrayList<Tile> grp){
+        int len = grp.size();
+        int tempInt = grp.get(0).getRank();
+        ArrayList<String> tempSuite = new ArrayList<String>();
+        tempSuite.add(grp.get(0).getSuite());
+
+        if(len == 3 || len == 4){
+            for(int i=1; i<len; i++){
+                if(grp.get(i).getRank() == tempInt && !tempSuite.contains(grp.get(i).getSuite())){
+                    tempSuite.add(grp.get(i).getSuite());
+                }
+                else return false;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
 }
