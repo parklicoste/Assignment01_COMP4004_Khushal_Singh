2721fee97090dcaaacb888f143add2169418b6b4
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-10-01 04:04:13 -0400

    TEST FOR Table class

--- src/main/java/project/TableTest.java
+++ src/main/java/project/TableTest.java
@@ -9,9 +9,25 @@ import java.util.Collections;
 
 public class TableTest {
 
+    Table t = new Table();
+
     @Test
     public void testTable() {
-        Table t = new Table();
+
         Assert.assertEquals(Collections.emptyList(), t.getPlayers()); // check for players on the table
+    }
+
+    @Test
+    public void testTablePlayers() {
+       Player p1 = new Player("p1");
+       Player p2 = new Player("p2");
+       Player p3 = new Player("p3");
+
+       t.insertPlayer(p1);
+       t.insertPlayer(p2);
+       t.insertPlayer(p3);
+
+        Assert.assertEquals(3, t.getPlayers().size()); // check for players on the table
+
     }
 }
